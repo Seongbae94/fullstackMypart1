0f5507c52ff71e700bc5a1797030e8ba53052233
Grimdal032 <kiriell0627@gmail.com>
2022-11-14 18:51:55 +0900

    뼈대 파일 생성, index - login - register 페이지 간 이동 가능한 기능 구현

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.q6bdrv2.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
+
+@app.route('/')
+def home():
+
+
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
